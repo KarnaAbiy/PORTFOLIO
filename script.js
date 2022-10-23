765fbc4f109a94b6a1517ea85ca0562e98a61d58
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-10-23 21:13:51 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,4 @@ button.addEventListener("click", () => {
     clicks += 1;
     score.innerHTML = clicks;
 })
-switch(score) {
-    case 5:
-        document.getElementById("image").src = "images/abiy5.jpg";
-        break;
-    case 10:
-        document.getElementById("image").src = "./sounds/ora.mp3";
-        break;
-    case 50:x
-        document.getElementById("image").src = "images/abiy50.jpg";
-        break;
-    case 100:
-        document.getElementById("image").src = "images/abiy100.jpg";
-        break;
-}  
 
