47069c974571b526712a1174936a3779123cf4be
Karmyshev Abiy <99629706+KarnaAbiy@users.noreply.github.com>
2022-10-27 23:16:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const button = document.querySelector("#button");
 const score = document.querySelector("#score");
 
 let clicks = 0;
 
 button.addEventListener("click", () => {
+    let color = "#";
+    color += Math.random().toString(16).slice(2,8);
+    score.style.color = color;
+
     clicks += 1;
     score.innerHTML = clicks;
-})
-
+})  
